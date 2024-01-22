76103c4f5f67f060e71ec7cf521b29ff53457a82
Pastushka <pastushka@MacBook-Pro-Tebe.local>
2024-01-22 21:06:17 +0600

    first commit

--- src/firstcode/Main.java
+++ src/firstcode/Main.java
@@ -1,3 +1,7 @@
+package firstcode;
+
+import java.util.Scanner;
+
 class Car {
     private int capacity;
 
@@ -12,14 +16,35 @@ class Car {
 
 class PassengerCar extends Car {
     private int numberOfPassengers;
+    private int maxPassengerCapacity;
+    private String[] passengerInfo;
 
-    public PassengerCar(int capacity, int numberOfPassengers) {
+    public PassengerCar(int capacity, int maxPassengerCapacity) {
         super(capacity);
-        this.numberOfPassengers = numberOfPassengers;
+        this.maxPassengerCapacity = maxPassengerCapacity;
+        this.passengerInfo = new String[maxPassengerCapacity];
+        this.numberOfPassengers = 0;
+    }
+
+    public void addPassengerInfo(String info) {
+        if (numberOfPassengers < maxPassengerCapacity) {
+            passengerInfo[numberOfPassengers++] = info;
+        } else {
+            System.out.println("Maximum passenger capacity reached for this car.");
+        }
     }
 
     public int getNumberOfPassengers() {
         return numberOfPassengers;
+    }
+
+    public void displayPassengerInfo() {
+        System.out.println("Passenger Information:");
+        for (String info : passengerInfo) {
+            if (info != null) {
+                System.out.println(info);
+            }
+        }
     }
 }
 
@@ -44,17 +69,43 @@ class Locomotive {
 class Train {
     private Locomotive locomotive;
     private Car[] cars;
+    private int maxPassengerCars;
+    private int maxFreightCars;
+    private int passengerCarCount;
+    private int freightCarCount;
 
-    public Train(Locomotive locomotive, Car[] cars) {
+    public Train(Locomotive locomotive, int maxPassengerCars, int maxFreightCars) {
         this.locomotive = locomotive;
-        this.cars = cars;
+        this.maxPassengerCars = maxPassengerCars;
+        this.maxFreightCars = maxFreightCars;
+        this.cars = new Car[maxPassengerCars + maxFreightCars];
+        this.passengerCarCount = 0;
+        this.freightCarCount = 0;
+    }
+
+    public void addPassengerCar(PassengerCar passengerCar) {
+        if (passengerCarCount < maxPassengerCars) {
+            cars[passengerCarCount++] = passengerCar;
+        } else {
+            System.out.println("Maximum passenger cars reached for this train.");
+        }
+    }
+
+    public void addFreightCar(FreightCar freightCar) {
+        if (freightCarCount < maxFreightCars) {
+            cars[maxPassengerCars + freightCarCount++] = freightCar;
+        } else {
+            System.out.println("Maximum freight cars reached for this train.");
+        }
     }
 
     public int calculateTotalCapacity() {
         int totalCapacity = locomotive.getPower();
 
         for (Car car : cars) {
-            totalCapacity += car.getCapacity();
+            if (car != null) {
+                totalCapacity += car.getCapacity();
+            }
         }
 
         return totalCapacity;
@@ -83,23 +134,30 @@ class Train {
 
         return freightWeight;
     }
-
 }
-
-
 
 public class Main {
     public static void main(String[] args) {
         Locomotive locomotive = new Locomotive(500);
-        PassengerCar passengerCar1 = new PassengerCar(50, 30);
-        PassengerCar passengerCar2 = new PassengerCar(40, 25);
+        PassengerCar passengerCar1 = new PassengerCar(50, 3);
+        PassengerCar passengerCar2 = new PassengerCar(40, 2);
         FreightCar freightCar1 = new FreightCar(100);
         FreightCar freightCar2 = new FreightCar(150);
 
-        Train train = new Train(locomotive, new Car[]{passengerCar1, passengerCar2, freightCar1, freightCar2});
+        Train train = new Train(locomotive, 5, 3);
+
+        train.addPassengerCar(passengerCar1);
+        train.addPassengerCar(passengerCar2);
+        train.addFreightCar(freightCar1);
+        train.addFreightCar(freightCar2);
 
         System.out.println("Total Capacity of the Train: " + train.calculateTotalCapacity());
         System.out.println("Total Passenger Count in the Train: " + train.calculatePassengerCount());
         System.out.println("Total Freight Weight in the Train: " + train.calculateFreightWeight());
+
+        passengerCar1.addPassengerInfo("John, 25, Male, From A to B");
+        passengerCar1.addPassengerInfo("Anna, 30, Female, From B to C");
+
+        passengerCar1.displayPassengerInfo();
     }
 }
